0cfde1765cc38c73091d9b302d91ae991b6e3001
2020819344@student.uitm.edu.my <2020819344@student.uitm.edu.my>
2022-06-16 01:39:52 +0800

    commit

--- register.php
+++ register.php
@@ -3,15 +3,12 @@
 <head>
 
 <?php include('functions.php') ?>
-//...
+
 
 <form method="post" action="register.php">
 	<?php echo display_error(); ?>
-//...
 </form>
 
-<input type="text" name="username" value="<?php echo $username; ?>">
-<input type="email" name="email" value="<?php echo $email; ?>">
 
 
 	<title>Registration system PHP and MySQL</title>
