d217eac185e041430b5d94ef88b80e5ebe412239
2020819344@student.uitm.edu.my <2020819344@student.uitm.edu.my>
2022-06-16 01:42:14 +0800

    coomit

--- functions.php
+++ functions.php
@@ -143,3 +143,13 @@ function login(){
 		}
 	}
 }
+
+// ...
+function isAdmin()
+{
+	if (isset($_SESSION['user']) && $_SESSION['user']['user_type'] == 'admin' ) {
+		return true;
+	}else{
+		return false;
+	}
+}
