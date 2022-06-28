6b1c392d3f1d04361f920fafbe93642b9f858c31
MohdFazly <2020819344@student.uitm.edu.my>
2022-06-29 01:25:39 +0800

    hghg

--- dataUserComplaint.php
+++ dataUserComplaint.php
@@ -9,18 +9,9 @@ values ('$_POST[fullname]','$_POST[email]','$_POST[comCategory]','$_POST[comDesc
 	if (!mysqli_query($conn,$sql)){
 		die ('Error: ' .mysqli_error($conn));
 	}
+
 echo "<script
 type='text/jscript'>alert('Commplaint has been submitted!')</script>";
 header('refresh:1 url=makecomplaint1.php');
-
-
-$sql="INSERT INTO users (fullname,address,date,email,password)
-values ('$_POST[fullname]','$_POST[address]','$_POST[date]','$_POST[email]','$_POST['password']')";
-	if (!mysqli_query($conn,$sql)){
-		die ('Error: ' .mysqli_error($conn));
-	}
-echo "<script
-type='text/jscript'>alert('Commplaint has been submitted!')</script>";
-header('refresh:1 url=login.php');
 
 ?>
