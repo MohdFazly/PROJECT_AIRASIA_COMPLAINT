85e724c931935ff01621758f4d40b23351ff49fd
MohdFazly <2020819344@student.uitm.edu.my>
2022-06-28 23:47:43 +0800

    gh

--- adminData.php
+++ adminData.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 include 'config.php';
 
 $sql="INSERT INTO adminn (fullname,email,address,password)
-values ('$_POST[fullname]','$_POST[email]','$_POST[address]','$_POST['password']')";
+values ('$_POST[fullname]','$_POST[email]','$_POST[address]','$_POST[password]')";
 	if (!mysqli_query($conn,$sql)){
 		die ('Error: ' .mysqli_error($conn));
 	}
