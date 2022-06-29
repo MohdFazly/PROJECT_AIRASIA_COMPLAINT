d34aa5f6ebe8e7c3a6850ce34622660072755da1
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-06-29 15:43:16 +0800

    fgfh

--- dataUserComplaint.php
+++ dataUserComplaint.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include 'config.php';
 
-$sql="INSERT INTO `usercomplaint` (`fullname`, `email`, `comCategory`,`flightNumber`, `from`, `to`, `comDescription`)
+$sql="INSERT INTO usercomplaint ('fullname', 'email', 'comCategory','flightNumber', 'from', 'to', 'comDescription')
  VALUES('$_POST[fullname]','$_POST[email]','$_POST[comCategory]','$_POST[flightNumber]', '$_POST[from]','$_POST[to]','$_POST[comDescription]')";
 	if (!mysqli_query($conn,$sql)){
 		die ('Error: ' .mysqli_error($conn));
