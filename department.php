b59853c61d64cbc64334bef55d72499963b858d9
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-06-29 08:49:29 +0800

    marhjdshjd

--- department.php
+++ department.php
@@ -10,11 +10,11 @@
 	<title>Make Complaint</title>
 </head>
 <body>
-<div class="" id="makecomplaint" method="post">
+<div class="" id="department" method="post">
 				<h4>Department<hr /></h4>
 				<form class="form" action="dataUserComplaint.php" method="POST" style="width:100%;">
 					<span id="span2">Department List:</span>
-					<select id="comCategory" name="comCategory" style="width:30%;border-radius:12px;height:37px;"required> 
+					<select id="DepartmentList" name="DepartmentList" style="width:30%;border-radius:12px;height:37px;"required> 
 						<option>Department Finance</option>
 						<option>Department Lanside Operation </option>
 						<option>Department Information Management</option>
@@ -26,7 +26,7 @@
 					<span id="span2">Date:</span>
 					<input type="date" name="date" class="form-control in2" placeholder="Please Enter you fullname..."required><br />
 					<span id="span">Details description:</span>
-					<textarea name="comDescription" class="form-control in1" placeholder="Feel free to write" style="height:200px;"required></textarea><br />
+					<textarea name="detailDescription" class="form-control in1" placeholder="Feel free to write" style="height:200px;"required></textarea><br />
 					
 					<input type="submit"  class="btn btn-success" name="btn-success" style="width:auto ;padding:10px;padding-left:50px;padding-right:50px;"  value="Send"><br />
 				</form>
