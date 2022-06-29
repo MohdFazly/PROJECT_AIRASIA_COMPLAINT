86dc6d1421869a8e66236d0c0caab6be03ac2164
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-06-29 20:05:57 +0800

    marla

--- dataUserComplaint.php
+++ dataUserComplaint.php
@@ -4,9 +4,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include 'config.php';
 
-$sql="INSERT INTO usercomplaint (fullname, email, comCategory, flightNumber, fromm, too, comDescription)
- VALUES('$_POST[fullname]','$_POST[email]','$_POST[comCategory]','$_POST[flightNumber]', '$_POST[fromm]','$_POST[too]','$_POST[comDescription]')";
+$sql="INSERT INTO usercomplaint ( fullname, email, comCategory, flightNumber, fromm, too, comDescription)
+ values ('$_POST[fullname]','$_POST[email]','$_POST[comCategory]','$_POST[flightNumber]', '$_POST[fromm]','$_POST[too]','$_POST[comDescription]')";
 	if (!mysqli_query($conn,$sql)){
+		
 		die ('Error: ' .mysqli_error($conn));
 	}
 
