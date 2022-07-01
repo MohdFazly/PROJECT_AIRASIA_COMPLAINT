b41596c35bafa86f91fabc528cd3d064b7c378b4
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-07-01 15:52:44 +0800

    margh

--- dataUserComplaint.php
+++ dataUserComplaint.php
@@ -5,14 +5,21 @@ ini_set('display_errors', 1);
 include 'config.php';
 
 $sql="INSERT INTO usercomplaint ( fullname, email, comCategory, flightNumber, fromm, too, comDescription)
- values ('$_POST[fullname]','$_POST[email]','$_POST[comCategory]','$_POST[flightNumber]', '$_POST[fromm]','$_POST[too]','$_POST[comDescription]')";
+values ('$_POST[fullname]','$_POST[email]','$_POST[comCategory]','$_POST[flightNumber]', '$_POST[fromm]','$_POST[too]','$_POST[comDescription]')";
 	if (!mysqli_query($conn,$sql)){
 		
 		die ('Error: ' .mysqli_error($conn));
 	}
 
+$sql="INSERT INTO department (departmentList ,fullname ,detailDescription)
+values ('$_POST[departmentList]','$_POST[fullname]','$_POST[detailDescription]')";
+		if (!mysqli_query($conn,$sql))
+		{
+			die ('Error: ' .mysqli_error($conn));
+		}
+
 echo "<script
-type='text/jscript'>alert('Commplaint has been submitted!')</script>";
+type='text/jscript'>alert('Complaint has been submitted!')</script>";
 header('refresh:1 url=makecomplaint1.php');
 
 ?>
