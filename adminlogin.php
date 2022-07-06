978af60cffa32d1c29a2914d10c594ef7aea89d3
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-07-06 16:57:26 +0800

    cc

--- adminlogin.php
+++ adminlogin.php
@@ -35,7 +35,7 @@
 	<div class="header">
 		<h2>Login: Admin</h2>
 	</div>
-	<form method="post" action="adminfunctions.php">
+	<form method="post" action="adminassign.php">
 
 		<?php include('errors.php'); ?>
 
