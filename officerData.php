a6d1294384733537c53a95ffe99424d178545f27
DESKTOP-17HJOIK\adik <m@3ky.my>
2022-07-06 15:43:16 +0800

    sdfs

--- officerData.php
+++ officerData.php
@@ -3,8 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 include 'config.php';
 
-$sql="INSERT INTO officer (fullname,email,address,password)
-values ('$_POST[fullname]','$_POST[email]','$_POST[address]','$_POST[password]')";
+$sql="INSERT INTO officer (fullname, hpnumber, email, departmentList, passwordd)
+values ('$_POST[fullname]','$_POST[hpnumber]','$_POST[email]','$_POST[departmentList]','$_POST[passwordd]')";
 	if (!mysqli_query($conn,$sql)){
 		die ('Error: ' .mysqli_error($conn));
 	}
